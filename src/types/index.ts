8b2ca6e4a6cf8ed3eb40fefad8a44192471137f8
Ilya Pirogov <ilja.pirogov@gmail.com>
2021-08-09 09:15:23 -0500

    Reimplemented with using mediasoup; more metrics; a lot of fixes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,14 @@
+import type {types} from "mediasoup-client";
 
 export interface WebRTCStatsConstructorOptions {
-    getStatsInterval: number
-    rawStats: boolean
-    statsObject: boolean
-    filteredStats: boolean
-    wrapGetUserMedia: boolean
-    debug: boolean
-    remote: boolean
-    logLevel: LogLevel
+    getStatsInterval: number;
+    rawStats?: boolean;
+    statsObject?: boolean;
+    filteredStats?: boolean;
+    wrapGetUserMedia?: boolean;
+    debug?: boolean;
+    remote?: boolean;
+    logLevel?: LogLevel;
 }
 
 /**
@@ -17,75 +18,118 @@ export interface WebRTCStatsConstructorOptions {
  * info: Informative messages including warnings and errors.
  * debug: Show everything including debugging information
  */
-export type LogLevel = 'none' | 'error' | 'warn' | 'info' | 'debug'
+export type LogLevel = "none" | "error" | "warn" | "info" | "debug";
 
-export type TimelineTag = 'getUserMedia' | 'peer' | 'connection' | 'track' | 'datachannel' | 'stats'
+export type TimelineTag = "getUserMedia" | "peer" | "connection" | "track" | "datachannel" | "stats";
+
+export interface TransportsInfo {
+    send?: types.Transport;
+    recv?: types.Transport;
+    sendPC?: RTCPeerConnection;
+    recvPC?: RTCPeerConnection;
+    peerId: string;
+}
 
 export interface TimelineEvent {
-    event: string
-    tag: TimelineTag
-    timestamp?: Date
-    data?: any
-    peerId?: string
-    error?: any
-    rawStats?: RTCStatsReport
-    statsObject?: any
-    filteredStats?: any
-
+    event: string;
+    tag: TimelineTag;
+    timestamp?: Date;
+    data?: any;
+    prev?: any;
+    peerId?: string;
+    error?: any;
+    rawStats?: RTCStatsReport[];
+    statsObject?: any;
+    filteredStats?: any;
+    appData?: any;
 }
 
 export interface AddPeerOptions {
-    pc: RTCPeerConnection
-    peerId: string
-    remote?: boolean
+    sendTransport?: types.Transport;
+    recvTransport?: types.Transport;
+    peerId: string;
+    remote?: boolean;
 }
 
 export interface GetUserMediaResponse {
-    constraints?: MediaStreamConstraints
-    stream?: MediaStream
-    error?: DOMError
+    constraints?: MediaStreamConstraints;
+    stream?: MediaStream;
+    error?: DOMError;
 }
 
 export interface MonitoredPeer {
-    pc: RTCPeerConnection
-    stream: MediaStream | null
-    stats: any
-    options: MonitorPeerOptions
+    info: TransportsInfo;
+    stream: MediaStream | null;
+    stats: any;
+    options: MonitorPeerOptions;
 }
 
 export interface MonitoredPeersObject {
-    [index: string]: MonitoredPeer
+    [index: string]: MonitoredPeer;
 }
 
-export interface TrackReport extends RTCStats {
-    bitrate?: number
-    packetRate?: number
+export interface TrackReport extends RTCTransportStats {
+    bitrate?: number | null;
+    packetRate?: number | null;
+    track?: MediaTrackSettings;
+    appData?: any;
+    [key: string]: any;
 }
 
 interface StatsObjectDetails {
-    inbound: TrackReport[]
-    outbound: TrackReport[]
+    inbound: TrackReport[];
+    outbound: TrackReport[];
 }
+
 export interface StatsObject {
-    audio: StatsObjectDetails
-    video: StatsObjectDetails
+    audio: StatsObjectDetails;
+    video: StatsObjectDetails;
     remote?: {
         audio: StatsObjectDetails
         video: StatsObjectDetails
-    }
-    connection: any
+    };
+    connection: any;
 }
 
 export interface CodecInfo {
-    clockRate: number
-    mimeType: number
-    payloadType: number
+    clockRate: number;
+    mimeType: number;
+    payloadType: number;
 }
 
 export interface MonitorPeerOptions {
-    remote: boolean
+    remote: boolean;
 }
 
 export interface ParseStatsOptions {
-    remote?: boolean
+    remote?: boolean;
+}
+
+export type PeerListener = (transport: types.Transport, pc: RTCPeerConnection, peerId: string, ev?: any) => void;
+
+export interface PeerListenersMap {
+    icecandidate: PeerListener;
+    track: PeerListener;
+    signalingstatechange: PeerListener;
+    iceconnectionstatechange: PeerListener;
+    icegatheringstatechange: PeerListener;
+    icecandidateerror: PeerListener;
+    connectionstatechange: PeerListener;
+    negotiationneeded: PeerListener;
+    datachannel: PeerListener;
+}
+
+export interface MediaTrackDetails {
+    settings: MediaTrackSettings;
+    constructorName: string;
+    capabilities: MediaTrackCapabilities;
+    kind: string;
+    contentHint: any;
+    _track: MediaStreamTrack;
+    readyState: "ended" | "live";
+    id: string;
+    label: string;
+    muted: boolean;
+    constraints: MediaTrackConstraints;
+    enabled: boolean;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,260 +1,263 @@
-import {StatsObject, CodecInfo, TrackReport, ParseStatsOptions} from './types/index'
+import {StatsObject, CodecInfo, TrackReport, ParseStatsOptions} from "./types/index";
+import {types} from "mediasoup-client";
 
 /**
  * A set of methods used to parse the rtc stats
  */
 
-function addAdditionalData (currentStats: StatsObject, previousStats?: StatsObject) {
-  // we need the previousStats stats to compute thse values
-  if (!previousStats) return currentStats
+function addAdditionalData(currentStats: StatsObject, previousStats: StatsObject | null): void {
+    // we need the previousStats stats to compute thse values
+    if (previousStats == null) {
+        return;
+    }
 
-  // audio
-  // inbound
-  currentStats.audio.inbound.map((report) => {
-    let prev = previousStats.audio.inbound.find(r => r.id === report.id)
-    report.bitrate = computeBitrate(report, prev, 'bytesReceived')
-    report.packetRate = computeBitrate(report, prev, 'packetsReceived')
-  })
-  // outbound
-  currentStats.audio.outbound.map((report) => {
-    let prev = previousStats.audio.outbound.find(r => r.id === report.id)
-    report.bitrate = computeBitrate(report, prev, 'bytesSent')
-    report.packetRate = computeBitrate(report, prev, 'packetsSent')
-  })
+    // audio
+    // inbound
+    currentStats.audio.inbound.map((report) => {
+        let prev = previousStats.audio.inbound.find(r => r.id === report.id);
+        report.bitrate = computeBitrate(report, prev, "bytesReceived");
+        report.packetRate = computeBitrate(report, prev, "packetsReceived");
+    });
+    // outbound
+    currentStats.audio.outbound.map((report) => {
+        let prev = previousStats.audio.outbound.find(r => r.id === report.id);
+        report.bitrate = computeBitrate(report, prev, "bytesSent");
+        report.packetRate = computeBitrate(report, prev, "packetsSent");
+    });
 
-  // video
-  // inbound
-  currentStats.video.inbound.map((report) => {
-    let prev = previousStats.video.inbound.find(r => r.id === report.id)
-    report.bitrate = computeBitrate(report, prev, 'bytesReceived')
-    report.packetRate = computeBitrate(report, prev, 'packetsReceived')
-  })
-  // outbound
-  currentStats.video.outbound.map((report) => {
-    let prev = previousStats.video.outbound.find(r => r.id === report.id)
-    report.bitrate = computeBitrate(report, prev, 'bytesSent')
-    report.packetRate = computeBitrate(report, prev, 'packetsSent')
-  })
-
-  return currentStats
+    // video
+    // inbound
+    currentStats.video.inbound.map((report) => {
+        let prev = previousStats.video.inbound.find(r => r.id === report.id);
+        report.bitrate = computeBitrate(report, prev, "bytesReceived");
+        report.packetRate = computeBitrate(report, prev, "packetsReceived");
+    });
+    // outbound
+    currentStats.video.outbound.map((report) => {
+        let prev = previousStats.video.outbound.find(r => r.id === report.id);
+        report.bitrate = computeBitrate(report, prev, "bytesSent");
+        report.packetRate = computeBitrate(report, prev, "packetsSent");
+    });
 }
 
-function getCandidatePairInfo (candidatePair, stats) {
-  if (!candidatePair || !stats) return {}
+function getCandidatePairInfo(candidatePair: RTCIceCandidatePairStats, stats: any) {
+    if (!candidatePair || !stats) return {};
 
-  const connection = {...candidatePair}
+    const connection: Record<string, unknown> = {...candidatePair};
 
-  if (connection.localCandidateId) {
-    const localCandidate = stats.get(connection.localCandidateId)
-    connection.local = {...localCandidate}
-  }
+    if (connection.localCandidateId) {
+        const localCandidate = stats.get(connection.localCandidateId);
+        connection.local = {...localCandidate};
+    }
 
-  if (connection.remoteCandidateId) {
-    const remoteCandidate = stats.get(connection.remoteCandidateId)
-    connection.remote = {...remoteCandidate}
-  }
+    if (connection.remoteCandidateId) {
+        const remoteCandidate = stats.get(connection.remoteCandidateId);
+        connection.remote = {...remoteCandidate};
+    }
 
-  return connection
+    return connection;
 }
 
 // Takes two stats reports and determines the rate based on two counter readings
 // and the time between them (which is in units of milliseconds).
-export function computeRate (newReport: TrackReport, oldReport: TrackReport, statName: string): number {
-  const newVal = newReport[statName]
-  const oldVal = oldReport ? oldReport[statName] : null
-  if (newVal === null || oldVal === null) {
-    return null
-  }
-  return (newVal - oldVal) / (newReport.timestamp - oldReport.timestamp) * 1000
+export function computeRate(newReport: TrackReport, oldReport: TrackReport | undefined, statName: keyof TrackReport): number | null {
+    const newVal = newReport[statName];
+    const oldVal = oldReport ? oldReport[statName] : null;
+    if (typeof newVal !== "number" || typeof oldVal !== "number" || newReport.timestamp == null || oldReport?.timestamp == null) {
+        return null;
+    }
+    return (newVal - oldVal) / (newReport.timestamp - oldReport.timestamp) * 1000;
 }
 
 // Convert a byte rate to a bit rate.
-export function computeBitrate (newReport: TrackReport, oldReport: TrackReport, statName: string): number {
-  return computeRate(newReport, oldReport, statName) * 8
+export function computeBitrate(newReport: TrackReport, oldReport: TrackReport | undefined, statName: keyof TrackReport): number | null {
+    const rate = computeRate(newReport, oldReport, statName);
+    return rate != null ? rate * 8 : null;
 }
 
-export function map2obj (stats: any) {
-  if (!stats.entries) {
-    return stats
-  }
-  const o = {}
-  stats.forEach(function (v, k) {
-    o[k] = v
-  })
-  return o
+export function map2obj(stats: any, appData: any, track: MediaTrackSettings | undefined, output: Record<string, any>) {
+    if (!stats.entries) {
+        return stats;
+    }
+    stats.forEach(function (v: unknown, k: string) {
+        output[k] = v;
+        output[k].appData = appData;
+        output[k].track = track;
+    });
 }
 
 // Enumerates the new standard compliant stats using local and remote track ids.
-export function parseStats (stats: any, previousStats: StatsObject | null, options: ParseStatsOptions | null = {}): StatsObject {
-  // Create an object structure with all the needed stats and types that we care
-  // about. This allows to map the getStats stats to other stats names.
+export function parseStats(stats: RTCStatsReport[], previousStats: StatsObject | null, options: ParseStatsOptions | null = {}): StatsObject {
+    // Create an object structure with all the needed stats and types that we care
+    // about. This allows to map the getStats stats to other stats names.
 
-  if (!stats) return null
+    /**
+     * The starting object where we will save the details from the stats report
+     * @type {Object}
+     */
+    let statsObject = {
+        audio: {
+            inbound: [],
+            outbound: [],
+        },
+        video: {
+            inbound: [],
+            outbound: [],
+        },
+        connection: {
+            inbound: [],
+            outbound: [],
+        },
+    } as StatsObject;
 
-  /**
-   * The starting object where we will save the details from the stats report
-   * @type {Object}
-   */
-  let statsObject = {
-    audio: {
-      inbound: [],
-      outbound: []
-    },
-    video: {
-      inbound: [],
-      outbound: []
-    },
-    connection: {
-      inbound: [],
-      outbound: []
+    // if we want to collect remote data also
+    if (options?.remote) {
+        statsObject.remote = {
+            audio: {
+                inbound: [],
+                outbound: [],
+            },
+            video: {
+                inbound: [],
+                outbound: [],
+            },
+        };
     }
-  } as StatsObject
 
-  // if we want to collect remote data also
-  if (options.remote) {
-    statsObject.remote = {
-      audio:{
-        inbound: [],
-        outbound: []
-      },
-      video:{
-        inbound: [],
-        outbound: []
-      }
+    for (const stat of stats) {
+        parseStatsInt(statsObject, stat, previousStats, options)
     }
-  }
 
-  for (const report of stats.values()) {
-    switch (report.type) {
-      case 'outbound-rtp': {
-        let outbound = {}
-        const mediaType = report.mediaType || report.kind
-        const codecInfo = {} as CodecInfo
-        if (!['audio', 'video'].includes(mediaType)) continue
+    return statsObject;
+}
 
-        if (report.codecId) {
-          const codec = stats.get(report.codecId)
-          if (codec) {
-            codecInfo.clockRate = codec.clockRate
-            codecInfo.mimeType = codec.mimeType
-            codecInfo.payloadType = codec.payloadType
-          }
-        }
-
-        statsObject[mediaType].outbound.push({...report, ...codecInfo})
-        break
-      }
-      case 'inbound-rtp': {
-        let inbound = {}
-        let mediaType = report.mediaType || report.kind
-        const codecInfo = {} as CodecInfo
-
-        // Safari is missing mediaType and kind for 'inbound-rtp'
-        if (!['audio', 'video'].includes(mediaType)) {
-          if (report.id.includes('Video')) mediaType = 'video'
-          else if (report.id.includes('Audio')) mediaType = 'audio'
-          else continue
-        }
-
-        if (report.codecId) {
-          const codec = stats.get(report.codecId)
-          if (codec) {
-            codecInfo.clockRate = codec.clockRate
-            codecInfo.mimeType = codec.mimeType
-            codecInfo.payloadType = codec.payloadType
-          }
-        }
-
-        // if we don't have connection details already saved
-        // and the transportId is present (most likely chrome)
-        // get the details from the candidate-pair
-        if (!statsObject.connection.id && report.transportId) {
-          const transport = stats.get(report.transportId)
-          if (transport && transport.selectedCandidatePairId) {
-            const candidatePair = stats.get(transport.selectedCandidatePairId)
-            statsObject.connection = getCandidatePairInfo(candidatePair, stats)
-          }
-        }
-
-        statsObject[mediaType].inbound.push({...report, ...codecInfo})
-        break
-      }
-      case 'peer-connection': {
-        statsObject.connection.dataChannelsClosed = report.dataChannelsClosed
-        statsObject.connection.dataChannelsOpened = report.dataChannelsOpened
-        break
-      }
-      case 'remote-inbound-rtp': {
-        if(!options.remote) break
-        let inbound = {}
-        let mediaType = report.mediaType || report.kind
-        const codecInfo = {} as CodecInfo
-
-        // Safari is missing mediaType and kind for 'inbound-rtp'
-        if (!['audio', 'video'].includes(mediaType)) {
-          if (report.id.includes('Video')) mediaType = 'video'
-          else if (report.id.includes('Audio')) mediaType = 'audio'
-          else continue
-        }
-
-        if (report.codecId) {
-          const codec = stats.get(report.codecId)
-          if (codec) {
-            codecInfo.clockRate = codec.clockRate
-            codecInfo.mimeType = codec.mimeType
-            codecInfo.payloadType = codec.payloadType
-          }
-        }
-
-        // if we don't have connection details already saved
-        // and the transportId is present (most likely chrome)
-        // get the details from the candidate-pair
-        if (!statsObject.connection.id && report.transportId) {
-          const transport = stats.get(report.transportId)
-          if (transport && transport.selectedCandidatePairId) {
-            const candidatePair = stats.get(transport.selectedCandidatePairId)
-            statsObject.connection = getCandidatePairInfo(candidatePair, stats)
-          }
-        }
-
-        statsObject.remote[mediaType].inbound.push({...report, ...codecInfo})
-        break
-      }
-      case 'remote-outbound-rtp': {
-        if(!options.remote) break
-        let outbound = {}
-        const mediaType = report.mediaType || report.kind
-        const codecInfo = {} as CodecInfo
-        if (!['audio', 'video'].includes(mediaType)) continue
-
-        if (report.codecId) {
-          const codec = stats.get(report.codecId)
-          if (codec) {
-            codecInfo.clockRate = codec.clockRate
-            codecInfo.mimeType = codec.mimeType
-            codecInfo.payloadType = codec.payloadType
-          }
-        }
-
-        statsObject.remote[mediaType].outbound.push({...report, ...codecInfo})
-        break
-      }
-      default:
-    }
-  }
-
-  // if we didn't find a candidate-pair while going through inbound-rtp
-  // look for it again
-  if (!statsObject.connection.id) {
+function parseStatsInt(statsObject: StatsObject, stats: any | null, previousStats: StatsObject | null, options: ParseStatsOptions | null = {}): void {
     for (const report of stats.values()) {
-      // select the current active candidate-pair report
-      if (report.type === 'candidate-pair' && report.nominated && report.state === 'succeeded') {
-        statsObject.connection = getCandidatePairInfo(report, stats)
-      }
+        switch (report.type) {
+            case "outbound-rtp": {
+                const mediaType: "video" | "audio" = report.mediaType || report.kind;
+                const codecInfo = {} as CodecInfo;
+                if (!["audio", "video"].includes(mediaType)) continue;
+
+                if (report.codecId) {
+                    const codec = stats.get(report.codecId);
+                    if (codec) {
+                        codecInfo.clockRate = codec.clockRate;
+                        codecInfo.mimeType = codec.mimeType;
+                        codecInfo.payloadType = codec.payloadType;
+                    }
+                }
+
+                statsObject[mediaType].outbound.push({...report, ...codecInfo});
+                break;
+            }
+            case "inbound-rtp": {
+                let mediaType: "audio" | "video" = report.mediaType || report.kind;
+                const codecInfo = {} as CodecInfo;
+
+                // Safari is missing mediaType and kind for 'inbound-rtp'
+                if (!["audio", "video"].includes(mediaType)) {
+                    if (report.id.includes("Video")) mediaType = "video";
+                    else if (report.id.includes("Audio")) mediaType = "audio";
+                    else continue;
+                }
+
+                if (report.codecId) {
+                    const codec = stats.get(report.codecId);
+                    if (codec) {
+                        codecInfo.clockRate = codec.clockRate;
+                        codecInfo.mimeType = codec.mimeType;
+                        codecInfo.payloadType = codec.payloadType;
+                    }
+                }
+
+                // if we don't have connection details already saved
+                // and the transportId is present (most likely chrome)
+                // get the details from the candidate-pair
+                if (!statsObject.connection.id && report.transportId) {
+                    const transport = stats.get(report.transportId);
+                    if (transport && transport.selectedCandidatePairId) {
+                        const candidatePair = stats.get(transport.selectedCandidatePairId);
+                        statsObject.connection = getCandidatePairInfo(candidatePair, stats);
+                    }
+                }
+
+                statsObject[mediaType].inbound.push({...report, ...codecInfo});
+                break;
+            }
+            case "peer-connection": {
+                statsObject.connection.dataChannelsClosed = report.dataChannelsClosed;
+                statsObject.connection.dataChannelsOpened = report.dataChannelsOpened;
+                break;
+            }
+            case "remote-inbound-rtp": {
+                if (!options?.remote) break;
+                let inbound = {};
+                let mediaType: "audio" | "video" = report.mediaType || report.kind;
+                const codecInfo = {} as CodecInfo;
+
+                // Safari is missing mediaType and kind for 'inbound-rtp'
+                if (!["audio", "video"].includes(mediaType)) {
+                    if (report.id.includes("Video")) mediaType = "video";
+                    else if (report.id.includes("Audio")) mediaType = "audio";
+                    else continue;
+                }
+
+                if (report.codecId) {
+                    const codec = stats.get(report.codecId);
+                    if (codec) {
+                        codecInfo.clockRate = codec.clockRate;
+                        codecInfo.mimeType = codec.mimeType;
+                        codecInfo.payloadType = codec.payloadType;
+                    }
+                }
+
+                // if we don't have connection details already saved
+                // and the transportId is present (most likely chrome)
+                // get the details from the candidate-pair
+                if (!statsObject.connection.id && report.transportId) {
+                    const transport = stats.get(report.transportId);
+                    if (transport && transport.selectedCandidatePairId) {
+                        const candidatePair = stats.get(transport.selectedCandidatePairId);
+                        statsObject.connection = getCandidatePairInfo(candidatePair, stats);
+                    }
+                }
+
+                statsObject.remote?.[mediaType].inbound.push({...report, ...codecInfo});
+                break;
+            }
+            case "remote-outbound-rtp": {
+                if (!options?.remote) break;
+                const mediaType: "audio" | "video" = report.mediaType || report.kind;
+                const codecInfo = {} as CodecInfo;
+                if (!["audio", "video"].includes(mediaType)) continue;
+
+                if (report.codecId) {
+                    const codec = stats.get(report.codecId);
+                    if (codec) {
+                        codecInfo.clockRate = codec.clockRate;
+                        codecInfo.mimeType = codec.mimeType;
+                        codecInfo.payloadType = codec.payloadType;
+                    }
+                }
+
+                statsObject.remote?.[mediaType].outbound.push({...report, ...codecInfo});
+                break;
+            }
+            default:
+        }
     }
-  }
 
-  statsObject = addAdditionalData(statsObject, previousStats)
+    // if we didn't find a candidate-pair while going through inbound-rtp
+    // look for it again
+    if (!statsObject.connection.id) {
+        for (const report of stats.values()) {
+            // select the current active candidate-pair report
+            if (report.type === "candidate-pair" && report.nominated && report.state === "succeeded" && (report.selected !== false)) {
+                statsObject.connection = getCandidatePairInfo(report, stats);
+            }
+        }
+    }
 
-  return statsObject
+    addAdditionalData(statsObject, previousStats ?? null);
 }
